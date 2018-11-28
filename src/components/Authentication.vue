d40237b62a4f3c1a38671c46d27c112729aad8e1
austin <austin89peterson@gmail.com>
2018-11-28 15:38:18 -0700

    Login page

--- src/components/Authentication.vue
+++ src/components/Authentication.vue
@@ -1,9 +1,32 @@
+<style>
+.login {
+    border-width: 15px;
+    border-style: solid;
+    border-color:black;
+    padding: 200px;
+    color:black;
+    background:orange;
+    height: fill;
+    width: fill;
+}
+</style>
+
 <template>
     <div id="login">
         <h3>this is the login page</h3>
     </div>
 </template>
 
+<template>
+    <div class= "login">
+        <h3>SIGN IN</h3>
+        <input type="text" placeholder="Email"><br>
+        <input type="password" placeholder="Password"><br>
+        <br>
+        <button>Connection</button>
+        <p> You don't have an account? You can create one</p>
+    </div>
+</template>
 
 <script>
 export default {
@@ -11,7 +34,8 @@ export default {
     data () {
         return {
         }
-    }
+    },
+    methods: {}
 }
 </script>
 
